8635a74ddb5909e3fa5241afd8b07f48fce9409f
mafhper <mafhper@gmail.com>
2021-12-06 09:57:00 -0300

    ⌨️ backend kick flip

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('<h1 style="color: blue">Hello World!</h1>')
+})
+ 
+app.listen(3000)
